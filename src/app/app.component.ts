7532355eb19ae645e1244650b299ed253ab42e9a
luckyvch1992 <luckyvch1992@gmail.com>
2023-03-20 11:10:38 +0200

    start ui implmentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+interface MediaFile {
+  id: string;
+  fileName: string;
+  mimeType: string;
+  fileSize: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'video-streaming-ui';
+  mediaFiles: MediaFile[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<MediaFile[]>('http://localhost:8080/storage/findAll').subscribe((mediaFiles: MediaFile[]) => {
+      this.mediaFiles = mediaFiles;
+    });
+  }
 }
